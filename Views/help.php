d93b29c2e0774dcd70e957c44af549780c1b9e92
Erduedidue <92919121+Erduedidue@users.noreply.github.com>
2021-11-18 00:12:07 +0100

    Update help.php

--- Views/help.php
+++ Views/help.php
@@ -3,11 +3,37 @@
 
 <?php if ($lang == "pl") { ?>
     
-<p>Polski tekst instrukcji. Do zrobienia</p>
+<p>Witamy w świecie Chloe!
+Chloe jest nieśmiała i spragniona bliskości młoda dziewczyna.
+Marek jest mężczyzną poznanym przez nią w Internecie.
+
+Gra ta polega wcieleniu się w postać Chloe i prowadzeniu rozmowy z Markiem. Podczas rozgrywki podejmujesz decyzje za którymi podążają pewne konsekwencje, którymi są zmiany w parametrach:
+•    Psychika -   – parametr ten oznacza stan psychiczny Chloe (im większy tym jej stan psychiczny jest lepszy) 
+•    Przywiązanie -   – parametr ten oznacza jak mocno Chloe się zbliżyła do Marka (im większy tym trudniej się od niego uwolnić)
+
+Podczas rozgrywki możesz również zaobserwować:
+•    W prawym dolnym okienku znajdują się myśli Chloe.
+•    Po lewo znajduje się czat rozmowy.
+•    W prawym górnym rogu znajduje się okienko z wyborami a zaraz pod nim są parametry.
+
+Podążaj za swoją intuicją i podejmuj właściwe decyzje. Czy uda ci się wyrwać z toksycznej relacji z Marek ? Czy pogrążysz się w niej? Sprawdź się!</p>
     
 <?php } else { ?>
 
-<p>English help. TODO</p>
+<p>Welcome to Chloe’s world!
+Chloe is a shy, young girl who wants closeness.
+Marcus is a man she met on the Internet.
+
+This game make you play as a Chloe and talking to Marcus. During the game, you make decisions that are followed by certain consequences, which are changes in parameters:
+•    Psyche -   - this parameter indicates Chloe's mental state (the higher her mental state is, the better)
+•    Attachment -   - this parameter means how much Chloe is close to Marcus (the bigger, the more difficult it is to get rid of him)
+
+During the game, you can also observe:
+•    In the lower right window are Chloe's thoughts.
+•    On the left is the chat.
+•    In the top right corner there is a window with choices and right below it are the parameters.
+
+Follow your intuition and make the right decisions. Can you break out of your toxic relationship with Marcus? Will you immerse yourself in it? Check yourself!</p>
     
 <?php } ?>
 
